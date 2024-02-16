720f71a33ec7aed38cf8a1b8a7f3717d93c2e3ea
JeanChristophe <119110069+JeanChristopheMoon@users.noreply.github.com>
2024-02-16 23:53:12 +0100

    Create K-MeansClustering.py

---
+++ K-MeansClustering.py
@@ -0,0 +1,4 @@
+#Cluster individuals based on their trust levels 
+or opinions on specific issues. This can reveal 
+patterns in how people perceive and trust different
+aspects, allowing for targeted analysis and communication strategies.
